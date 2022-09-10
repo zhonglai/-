0d001d08b9f54b5805c50a80b135e76a6c2c586d
zhonglai <2895029483@qq.com>
2022-09-11 02:18:52 +0800

    splint编译，生成文件放在out目录

---
+++ config.h
@@ -0,0 +1,109 @@
+/* config.h.  Generated from config.hin by configure.  */
+/* config.hin.  Generated from configure.ac by autoheader.  */
+
+/*@-constmacros@*/
+
+/* build */
+#define BUILD "x86_64-unknown-linux-gnu"
+
+/* build_alias */
+#define BUILD_ALIAS ""
+
+/* build_cpu */
+#define BUILD_CPU "x86_64"
+
+/* build_os */
+#define BUILD_OS "linux-gnu"
+
+/* build_vendor */
+#define BUILD_VENDOR "unknown"
+
+/* The directory where Splint's library files live */
+#define DEFAULT_LARCHPATH ".:/usr/local/splint/share/splint/lib:/home/wangzhonglai/github/splint/splint-3.1.1/lib:"
+
+/* Splint's imports directory */
+#define DEFAULT_LCLIMPORTDIR ".:/usr/local/splint/share/splint/imports:/home/wangzhonglai/github/splint/splint-3.1.1/imports"
+
+/* system include directory */
+#define GCC_INCLUDE_DIR "/usr/include"
+
+/* alternate include directory */
+#define GCC_INCLUDE_DIR2 "/usr/local/splint/include"
+
+/* host */
+#define HOST "x86_64-unknown-linux-gnu"
+
+/* host_alias */
+#define HOST_ALIAS ""
+
+/* host_cpu */
+#define HOST_CPU "x86_64"
+
+/* host_os */
+#define HOST_OS "linux-gnu"
+
+/* host_vendor */
+#define HOST_VENDOR "unknown"
+
+/* String describing who compiled this binary and how */
+#define LCL_COMPILE "Compiled using gcc -g -O2 on Linux shell 5.13.0-48-generic #54~20.04.1-Ubuntu SMP Thu Jun 2 23:37:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux by wangzhonglai"
+
+/* Splint's version number */
+#define LCL_PARSE_VERSION "Splint 3.1.1"
+
+/* Define to disable support LCL files */
+/* #undef NOLCL */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "splint-bug@splint.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Splint"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Splint 3.1.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "splint"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.1.1"
+
+/* Splint's version number and configure/build date */
+#define SPLINT_VERSION "Splint 3.1.1 --- 11 Sep 2022"
+
+/* The system's main include directory */
+#define SYSTEM_LIBDIR "/usr/local/splint/include"
+
+/* target */
+#define TARGET "x86_64-unknown-linux-gnu"
+
+/* target_alias */
+#define TARGET_ALIAS ""
+
+/* target_cpu */
+#define TARGET_CPU "x86_64"
+
+/* target_os */
+#define TARGET_OS "linux-gnu"
+
+/* target_vendor */
+#define TARGET_VENDOR "unknown"
+
+/* uname output */
+#define UNAME "Linux"
+
+/* Define if you're on a Unixy system */
+#define UNIX 1
+
+/* Define to 1 if `lex' declares `yytext' as a `char *' by default, not a
+   `char[]'. */
+#define YYTEXT_POINTER 1
+
+/* needed to expose mode_t on some machines */
+/* #undef _ALL_SOURCE */
+
+/*@=constmacros@*/
